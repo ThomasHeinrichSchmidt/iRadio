6710d43da39d0b09c89739c06c09abef59c42ddd
ThomasHeinrichSchmidt <justmaybe@go4more.de>
2020-05-07 18:08:36 +0200

    Try to add <Root> to Telnet.xml by using a RootStreamReader

--- iRadio/Program.cs
+++ iRadio/Program.cs
@@ -30,9 +30,10 @@ namespace iRadio
                   </Child>  
                 </Root>";
 
-            string markup = @"<iRadio> 
+            string markup = @"<Root> 
                                 <update id=""play"" > <value id =""timep"" min=""0"" max=""65535"" > 1698 </value > </update >  
-                             </iRadio>";
+                                <update id=""play"" > <value id =""timep"" min=""0"" max=""65535"" > 1699 </value > </update >  
+                             </Root>";   //  if missing: unexpected end of file. Elements not closed: Root.
 
             IEnumerable <string> grandChildData =
                 from el in StreamRootChildDoc(new StringReader(markup_child))
@@ -44,8 +45,9 @@ namespace iRadio
                 where (string)el.Attribute("id") == "play"
                 select (string)el.Element("value");
 
+            RootStreamReader TelnetFile = new RootStreamReader("Telnet.xml");
             IEnumerable<string> iRadioData =
-                from el in StreamiRadioDoc(new StreamReader("Telnet.xml"))
+                from el in StreamiRadioDoc(TelnetFile)
                 where (string)el.Attribute("id") == "play"
                 select (string)el.Element("value");
 
@@ -88,6 +90,7 @@ namespace iRadio
                 }
             }
         }
+
         static IEnumerable<XElement> StreamRootChildDoc(StringReader stringReader)
         {
             using (System.Xml.XmlReader reader = System.Xml.XmlReader.Create(stringReader))
@@ -110,5 +113,100 @@ namespace iRadio
                 }
             }
         }
+    }
+
+
+    // https://stackoverflow.com/questions/56260094/is-it-possible-to-alter-the-node-value-with-xmlreader
+    class RootedXmlReader : XmlReader
+    {
+        private XmlReader _reader;
+        private XmlReader _root;
+        private bool start = false;
+        private string rootnode = "<Root>";
+
+        private RootedXmlReader(TextReader reader)
+        {
+            _reader = XmlReader.Create(reader);
+            _root = XmlReader.Create(rootnode);
+        }
+        public override bool Read()
+        {
+            if (start)
+            {
+                start = false;
+                return _root.Read();
+            }
+            else
+            {
+                return _reader.Read();
+            }
+        }
+        #region Wrapper Boilerplate
+        public override XmlNodeType NodeType => _reader.NodeType;
+        public override string LocalName => _reader.LocalName;
+        public override string NamespaceURI => _reader.NamespaceURI;
+        public override string Prefix => _reader.Prefix;
+        public override string Value => _reader.Value;
+        public override int Depth => _reader.Depth;
+        public override string BaseURI => _reader.BaseURI;
+        public override bool IsEmptyElement => _reader.IsEmptyElement;
+        public override int AttributeCount => _reader.AttributeCount;
+        public override bool EOF => _reader.EOF;
+        public override ReadState ReadState => _reader.ReadState;
+        public override XmlNameTable NameTable => _reader.NameTable;
+        public override string GetAttribute(string name) => _reader.GetAttribute(name);
+        public override string GetAttribute(string name, string namespaceURI) => _reader.GetAttribute(name, namespaceURI);
+        public override string GetAttribute(int i) => _reader.GetAttribute(i);
+        public override string LookupNamespace(string prefix) => _reader.LookupNamespace(prefix);
+        public override bool MoveToAttribute(string name) => _reader.MoveToAttribute(name);
+        public override bool MoveToAttribute(string name, string ns) => _reader.MoveToAttribute(name, ns);
+        public override bool MoveToElement() => _reader.MoveToElement();
+        public override bool MoveToFirstAttribute() => _reader.MoveToFirstAttribute();
+        public override bool MoveToNextAttribute() => _reader.MoveToNextAttribute();
+        public override bool ReadAttributeValue() => _reader.ReadAttributeValue();
+        public override void ResolveEntity() => _reader.ResolveEntity();
+        #endregion Wrapper Boilerplate
+    }
+
+    class RootStreamReader : StreamReader
+    {
+        public RootStreamReader(string path)
+          : base(path)
+        {
+        }
+
+        private bool start = false;
+        private string rootnode = "<Root>";
+        private int rootnodecount = 0;
+
+        public override string ReadLine()
+        {
+            if (start)
+            {
+                start = false;
+                return rootnode + base.ReadLine();
+            }
+            else {
+                return base.ReadLine();
+            }
+        }
+
+        public override int Read()
+        {
+            if (start)
+            {
+                if (rootnodecount >= rootnode.Length)
+                {
+                    start = false;
+                }
+                return (int) rootnode[rootnodecount++];
+
+            }
+            else
+            {
+                return base.Read();
+            }
+        }
+
     }
 }
