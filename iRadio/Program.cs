f6019a55906f6225f1bd156c0e0b9960a199cf09
ThomasHeinrichSchmidt <justmaybe@go4more.de>
2020-05-07 22:44:01 +0200

    first working version, XML read as fragment

--- iRadio/Program.cs
+++ iRadio/Program.cs
@@ -30,17 +30,10 @@ namespace iRadio
                   </Child>  
                 </Root>";
 
-            string markup = @"  <update id=""play"" > <value id =""timep"" min=""0"" max=""65535"" > 1698 </value > </update >  
+            string markup = @"
+                                <update id=""play"" > <value id =""timep"" min=""0"" max=""65535"" > 1698 </value > </update >  
                                 <update id=""play"" > <value id =""timep"" min=""0"" max=""65535"" > 1699 </value > </update >  
                              ";   //  if missing: unexpected end of file. Elements not closed: Root.
-
-            // string rootnode = "<Root>  </Root>";
-            XmlReader _root = RootedXmlReader.Create(new StringReader(markup));
-            _root.Read();
-            _root.Read();
-            _root.Read();
-            _root.Read();
-
 
             IEnumerable<string> grandChildData =
                 from el in StreamRootChildDoc(new StringReader(markup_child))
@@ -77,7 +70,8 @@ namespace iRadio
 
         static IEnumerable<XElement> StreamiRadioDoc(TextReader stringReader)
         {
-            using (XmlReader reader = RootedXmlReader.Create(stringReader))
+            var settings = new XmlReaderSettings { ConformanceLevel = ConformanceLevel.Fragment };
+            using (XmlReader reader = XmlReader.Create(stringReader, settings))
             {
                 // reader.MoveToContent();
                 while (reader.Read())
@@ -119,67 +113,5 @@ namespace iRadio
                 }
             }
         }
-    }
-
-
-    // https://stackoverflow.com/questions/56260094/is-it-possible-to-alter-the-node-value-with-xmlreader
-    class RootedXmlReader : XmlReader
-    {
-        private XmlReader _reader;
-        private XmlReader _root;
-        private static bool start = true;
-        private static bool root = false;
-        private string rootnode = "<Root>  </Root>";
-
-        private RootedXmlReader(TextReader reader)
-        {
-            _reader = XmlReader.Create(reader);
-            _root = XmlReader.Create(new StringReader(rootnode));
-        }
-        public static new XmlReader Create(TextReader input)
-        {
-            start = true;
-            root = false;
-            return new RootedXmlReader(input);
-        }
-
-        public override bool Read()
-        {
-            if (root) start = false;
-            if (start)
-            {
-                root = true;
-                return _root.Read();
-            }
-            else
-            {
-                return _reader.Read();
-            }
-        }
-        #region Wrapper Boilerplate
-        public override XmlNodeType NodeType => (start) ? _root.NodeType : _reader.NodeType;
-        public override string LocalName => (start) ? _root.LocalName : _reader.LocalName;
-        public override string NamespaceURI => (start) ? _root.NamespaceURI : _reader.NamespaceURI;
-        public override string Prefix => (start) ? _root.Prefix : _reader.Prefix;
-        public override string Value => (start) ? _root.Value : _reader.Value;
-        public override int Depth => (start) ? _root.Depth : _reader.Depth;
-        public override string BaseURI => (start) ? _root.BaseURI : _reader.BaseURI;
-        public override bool IsEmptyElement => (start) ? _root.IsEmptyElement : _reader.IsEmptyElement;
-        public override int AttributeCount => (start) ? _root.AttributeCount : _reader.AttributeCount;
-        public override bool EOF => (start) ? _root.EOF : _reader.EOF;
-        public override ReadState ReadState => (start) ? _root.ReadState : _reader.ReadState;
-        public override XmlNameTable NameTable => (start) ? _root.NameTable : _reader.NameTable;
-        public override string GetAttribute(string name) => (start) ? _root.GetAttribute(name) : _reader.GetAttribute(name);
-        public override string GetAttribute(string name, string namespaceURI) => (start) ? _root.GetAttribute(name, namespaceURI) : _reader.GetAttribute(name, namespaceURI);
-        public override string GetAttribute(int i) => (start) ? _root.GetAttribute(i) : _reader.GetAttribute(i);
-        public override string LookupNamespace(string prefix) => (start) ? _root.LookupNamespace(prefix) : _reader.LookupNamespace(prefix);
-        public override bool MoveToAttribute(string name) => (start) ? _root.MoveToAttribute(name) : _reader.MoveToAttribute(name);
-        public override bool MoveToAttribute(string name, string ns) => (start) ? _root.MoveToAttribute(name, ns) : _reader.MoveToAttribute(name, ns);
-        public override bool MoveToElement() => (start) ? _root.MoveToElement() : _reader.MoveToElement();
-        public override bool MoveToFirstAttribute() => (start) ? _root.MoveToFirstAttribute() : _reader.MoveToFirstAttribute();
-        public override bool MoveToNextAttribute() => (start) ? _root.MoveToNextAttribute() : _reader.MoveToNextAttribute();
-        public override bool ReadAttributeValue() => (start) ? _root.ReadAttributeValue() : _reader.ReadAttributeValue();
-        public override void ResolveEntity() => _reader.ResolveEntity();  // (start) ? _root.ResolveEntity() : _reader.ResolveEntity();
-        #endregion Wrapper Boilerplate
     }
 }
